95b72a413e8b6e6223c437506050ef5c0b675c5f
Reinaldo Dorti <reinaldorti@gmail.com>
2020-10-17 17:51:17 -0300

    corrigindo o tempo de sessao no controlador dash com o metado dashboard e upando o banco novo

--- codigoaberto_novo.sql
+++ codigoaberto_novo.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 05-Jul-2020 às 16:48
--- Versão do servidor: 10.4.11-MariaDB
--- versão do PHP: 7.4.6
+-- Tempo de geração: 17/10/2020 às 17:23
+-- Versão do servidor: 10.4.14-MariaDB
+-- Versão do PHP: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `codigoaberto`
+-- Banco de dados: `codigoaberto_novo`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `address`
+-- Estrutura para tabela `address`
 --
 
 CREATE TABLE `address` (
@@ -45,7 +45,7 @@ CREATE TABLE `address` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `categories`
+-- Estrutura para tabela `categories`
 --
 
 CREATE TABLE `categories` (
@@ -61,7 +61,7 @@ CREATE TABLE `categories` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `posts`
+-- Estrutura para tabela `posts`
 --
 
 CREATE TABLE `posts` (
@@ -85,7 +85,25 @@ CREATE TABLE `posts` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `users`
+-- Estrutura para tabela `slides`
+--
+
+CREATE TABLE `slides` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `status` varchar(20) COLLATE utf8_unicode_ci DEFAULT '1' COMMENT '1 ativo, 2 inativo',
+  `title` varchar(255) COLLATE utf8_unicode_ci DEFAULT '',
+  `uri` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `cover` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `subtitle` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `post_at` date DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `users`
 --
 
 CREATE TABLE `users` (
@@ -114,24 +132,24 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Índices para tabelas despejadas
+-- Índices de tabelas apagadas
 --
 
 --
--- Índices para tabela `address`
+-- Índices de tabela `address`
 --
 ALTER TABLE `address`
   ADD PRIMARY KEY (`id`),
   ADD KEY `address_user_id_foreign` (`user_id`);
 
 --
--- Índices para tabela `categories`
+-- Índices de tabela `categories`
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices para tabela `posts`
+-- Índices de tabela `posts`
 --
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`),
@@ -140,7 +158,14 @@ ALTER TABLE `posts`
 ALTER TABLE `posts` ADD FULLTEXT KEY `title` (`title`);
 
 --
--- Índices para tabela `users`
+-- Índices de tabela `slides`
+--
+ALTER TABLE `slides`
+  ADD PRIMARY KEY (`id`);
+ALTER TABLE `slides` ADD FULLTEXT KEY `title` (`title`);
+
+--
+-- Índices de tabela `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
@@ -150,7 +175,7 @@ ALTER TABLE `users` ADD FULLTEXT KEY `first_name_2` (`first_name`,`last_name`,`e
 ALTER TABLE `users` ADD FULLTEXT KEY `first_name_3` (`first_name`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT de tabelas apagadas
 --
 
 --
@@ -172,23 +197,29 @@ ALTER TABLE `posts`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de tabela `slides`
+--
+ALTER TABLE `slides`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- Restrições para despejos de tabelas
+-- Restrições para dumps de tabelas
 --
 
 --
--- Limitadores para a tabela `address`
+-- Restrições para tabelas `address`
 --
 ALTER TABLE `address`
   ADD CONSTRAINT `address_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
--- Limitadores para a tabela `posts`
+-- Restrições para tabelas `posts`
 --
 ALTER TABLE `posts`
   ADD CONSTRAINT `posts_author_foreign` FOREIGN KEY (`author`) REFERENCES `users` (`id`) ON DELETE CASCADE,
